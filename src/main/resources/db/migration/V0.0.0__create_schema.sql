6938a9245de97832e965e7445b8b2bc70c0065e4
Fabio Heer <fabio.heer@dvbern.ch>
2019-08-27 14:56:35 +0200

    chore: create schema through FlyWay
    There seem to be some race condition issues when combining hibernate
    drop--and-create and FlyWay. Better create the whole schema with FlyWay
    and only use hibernate to update the schema during development.

---
+++ src/main/resources/db/migration/V0.0.0__create_schema.sql
@@ -0,0 +1,70 @@
+CREATE TABLE client (
+	id            BIGSERIAL    NOT NULL
+		CONSTRAINT client_pkey PRIMARY KEY,
+	clientid      VARCHAR(255) NOT NULL,
+	grantedsince  TIMESTAMP    NOT NULL,
+	institutionid VARCHAR(255) NOT NULL,
+	CONSTRAINT client_uc1
+		UNIQUE (clientid, institutionid)
+);
+
+CREATE INDEX client_idx1
+	ON client(clientid, institutionid, grantedsince);
+
+CREATE TABLE consumedmessage (
+	eventid         UUID      NOT NULL
+		CONSTRAINT consumedmessage_pkey
+			PRIMARY KEY,
+	timeofreceiving TIMESTAMP NOT NULL
+);
+
+CREATE TABLE verfuegung (
+	id                       BIGSERIAL    NOT NULL
+		CONSTRAINT verfuegung_pkey
+			PRIMARY KEY,
+	betreuungsart            VARCHAR(255) NOT NULL,
+	bis                      DATE         NOT NULL,
+	gesuchsteller            JSONB        NOT NULL,
+	ignoriertezeitabschnitte JSONB        NOT NULL,
+	institutionid            VARCHAR(255) NOT NULL,
+	kind                     JSONB        NOT NULL,
+	refnr                    VARCHAR(255) NOT NULL,
+	verfuegtam               TIMESTAMP    NOT NULL,
+	version                  INTEGER      NOT NULL
+		CONSTRAINT verfuegung_version_check
+			CHECK (version >= 0),
+	von                      DATE         NOT NULL,
+	zeitabschnitte           JSONB        NOT NULL
+);
+
+CREATE TABLE clientverfuegung (
+	id            BIGSERIAL    NOT NULL
+		CONSTRAINT clientverfuegung_pkey
+			PRIMARY KEY,
+	clientid      VARCHAR(255) NOT NULL,
+	institutionid VARCHAR(255) NOT NULL,
+	since         TIMESTAMP    NOT NULL,
+	verfuegung_id BIGINT       NOT NULL
+		CONSTRAINT verfuegung_fk
+			REFERENCES verfuegung
+);
+
+CREATE INDEX clientverfuegung_idx1
+	ON clientverfuegung(clientid, since, id);
+
+CREATE INDEX verfuegung_idx1
+	ON verfuegung(institutionid, verfuegtam);
+
+CREATE TABLE institution (
+	id            VARCHAR(255) NOT NULL
+		CONSTRAINT institution_pkey
+			PRIMARY KEY,
+	adresszusatz  VARCHAR(255),
+	hausnummer    VARCHAR(255),
+	land          VARCHAR(255),
+	ort           VARCHAR(255) NOT NULL,
+	plz           VARCHAR(255) NOT NULL,
+	strasse       VARCHAR(255) NOT NULL,
+	name          VARCHAR(255),
+	traegerschaft VARCHAR(255)
+);
