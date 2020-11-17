9e2287861589d6647d928e22b43ac1c19ef1f200
Fabio Heer <fabio.heer@dvbern.ch>
2020-11-17 12:06:13 +0100

    KIBON-1653 refactor: filter Verfuegungen by client Gueltigkeit range
    A client shall only get Verfuegungen in the time range it's allowed to.
    A Verfuegung for a period 2020/2021 may not be loaded from a client with
    permission to get data after 2021-07-31.

--- src/test/java/ch/dvbern/kibon/verfuegung/service/VerfuegungServiceQuarkusTest.java
+++ src/test/java/ch/dvbern/kibon/verfuegung/service/VerfuegungServiceQuarkusTest.java
@@ -17,9 +17,10 @@
 
 package ch.dvbern.kibon.verfuegung.service;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 
 import ch.dvbern.kibon.clients.service.ClientService;
@@ -28,9 +29,12 @@ import ch.dvbern.kibon.testutils.TestcontainersEnvironment;
 import ch.dvbern.kibon.testutils.TransactionHelper;
 import ch.dvbern.kibon.verfuegung.model.ClientVerfuegungDTO;
 import ch.dvbern.kibon.verfuegung.service.filter.ClientVerfuegungFilter;
+import io.quarkus.test.TestTransaction;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.allOf;
@@ -53,14 +57,15 @@ class VerfuegungServiceQuarkusTest {
 	@Inject
 	TransactionHelper tx;
 
-	// Instead of using the @Transactional annotation or @TransactionalQuarkusTest we start the transactions manually.
-	// Otherwise only one transaction for the whole test is started, such that the second call of on verfuegungService
-	// returns excatly the same result as the first.
+	// Use @TestTransaction, to rollback changes once the test method is complete.
+	// Multiple transactions are executed manually. Otherwise only one transaction for the whole test is started, such
+	// that the second call of on verfuegungService returns excatly the same result as the first.
+	@TestTransaction
 	@Test
 	public void testGetAllForClient_doesNotReturnInactiveClients() throws Exception {
 		ClientVerfuegungFilter kitAdmin = new ClientVerfuegungFilter("kitAdmin");
 
-		// verify setup: when importing import-dev.sql, there should be 400 entries for client kitAdmin with
+		// verify setup: when importing import-test.sql, there should be 400 entries for client kitAdmin with
 		// institution IDs 1 or 2
 		assertThat(
 			tx.newTransaction(() -> verfuegungService.getAllForClient(kitAdmin)),
@@ -68,7 +73,8 @@ class VerfuegungServiceQuarkusTest {
 		);
 
 		// deactivate the client kitAdmin for institutionId 1
-		InstitutionClientEventDTO dto = new InstitutionClientEventDTO("1", "kitAdmin", "EXCHANGE_SERVICE_USER", null, null);
+		InstitutionClientEventDTO dto =
+			new InstitutionClientEventDTO("1", "kitAdmin", "EXCHANGE_SERVICE_USER", null, null);
 		tx.newTransaction(() -> clientService.onClientRemoved(dto));
 
 		// there should be no more results for insitutionId 1
@@ -77,8 +83,37 @@ class VerfuegungServiceQuarkusTest {
 			allForClient,
 			allOf(hasSize(100), everyItem(hasProperty("institutionId", is("2"))))
 		);
+	}
 
-		// restore client (to make sure following integration tests get the expected DB setup)
-		tx.newTransaction(() -> clientService.onClientAdded(dto, LocalDateTime.now()));
+	@ParameterizedTest
+	@CsvSource(value = {
+		"NULL, NULL, 400",
+		"NULL, 2019-07-31, 100",
+		"2020-08-01, NULL, 100",
+		"2000-01-01, NULL, 400",
+		"2020-01-01, 2020-01-31, 400",
+		"2020-08-01, 2022-07-31, 100"
+	},
+		nullValues = "NULL")
+	void testGetAllForClient_filtersByClientGueltigkeit(
+		@Nullable LocalDate von,
+		@Nullable LocalDate bis,
+		int expectedResults) throws Exception {
+
+		ClientVerfuegungFilter kitAdmin = new ClientVerfuegungFilter("kitAdmin");
+
+		// change gueltigkeit of client
+		InstitutionClientEventDTO dto =
+			new InstitutionClientEventDTO("1", "kitAdmin", "EXCHANGE_SERVICE_USER", von, bis);
+		tx.newTransaction(() -> clientService.onClientModified(dto));
+
+		// there should be no more results for insitutionId 1
+		List<ClientVerfuegungDTO> allForClient = tx.newTransaction(() -> verfuegungService.getAllForClient(kitAdmin));
+		assertThat(allForClient, hasSize(expectedResults));
+
+		// restore gueltigkeit of client (@TestTransaction and @ParametrizedTest does not seem to work)
+		dto.setGueltigAb(null);
+		dto.setGueltigBis(null);
+		tx.newTransaction(() -> clientService.onClientModified(dto));
 	}
 }
